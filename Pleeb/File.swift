bf37cb9aa6b7cfac9fcf5eb76c7fb53c92c120d2
Brent Kirkland <brentckirkland@gmail.com>
2015-03-05 23:04:35 -0800

    removed deleted files

--- Pleeb/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  Pleeb
-//
-//  Created by Brent Kirkland on 3/1/15.
-//  Copyright (c) 2015 Brent Kirkland. All rights reserved.
-//
-
-import Foundation
--- Pleeb/LeftViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LeftViewController.swift
-//  Pleeb
-//
-//  Created by Brent Kirkland on 3/1/15.
-//  Copyright (c) 2015 Brent Kirkland. All rights reserved.
-//
-
-import Foundation
